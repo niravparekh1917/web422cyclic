77f7e9fb1a9bb305a91628e0d91390558c164076
Nirav Parekh <niravparekh1917@gmail.com>
2022-09-16 13:41:41 -0400

    commit

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+
+/********************************************************************************* 
+ * WEB422 – Assignment 1 
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+ * No part of this assignment has been copied manually or electronically from any other source * (including web sites) or distributed to other students. * 
+ * * Name: ______________________ Student ID: ______________ Date: ________________ 
+ * Cyclic Link: _______________________________________________________________ * 
+ ********************************************************************************/
+
 const express = require("express");
 const path = require("path");
 const bodyParser = require('body-parser');
